679ca21acb0342643728fd1d989b04c5eae6db9a
ion <delion.dev@gmail.com>
2025-11-08 09:44:10 +0900

    feat/#1: Home 및 Test 컴포넌트 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center bg-amber-400">
+      <p>ReactComponents</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Test.tsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div className="flex justify-center items-center bg-amber-400">
+      <p>Test ReactComponents</p>
+    </div>
+  );
+};
+
+export default Test;
