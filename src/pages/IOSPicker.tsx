cbc90af017c032814c4239ea01f9763fff23ac1c
ion <delion.dev@gmail.com>
2025-12-26 03:31:38 +0900

    feat/#3: IOSPicker 컴포넌트에 Picker 렌더링 추가

--- src/pages/IOSPicker.tsx
+++ src/pages/IOSPicker.tsx
@@ -1,7 +1,11 @@
+import Picker from "@/components/ios-picker/Picker";
+
 const IOSPicker = () => {
   return (
     <>
       <h3 className="bg-amber-400">IOSPicker ReactComponents</h3>
+
+      <Picker />
     </>
   );
 };
